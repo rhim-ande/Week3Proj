e3779bbd0e4efd213f4fda96e3c69ca2d4a6d016
Rhim Andemichael <rhima@princeton.edu>
2022-07-12 21:42:51 +0000

    add templates folder

--- test/test_basic.py
+++ test/test_basic.py
@@ -21,5 +21,7 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/register', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    
+
 if __name__ == "__main__":
     unittest.main()
--- test/test_users.py
+++ test/test_users.py
@@ -31,10 +31,13 @@ class UsersTests(unittest.TestCase):
     def test_invalid_username_registration(self):
         response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
         self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+        
 
     def test_invalid_email_registration(self):
         response = self.register('test2', 'test@example', 'FlaskIsAwesome')
         self.assertIn(b'Invalid email address.', response.data)
+
+    
 
 
 if __name__ == "__main__":
